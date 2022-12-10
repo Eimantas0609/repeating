64c3b145fc85b3deca62b9934924dd8d3abca5f1
Eimantas <jurenkovai@gmail.com>
2022-12-10 16:49:37 +0200

    mini ref

--- 01_hw/script.js
+++ 01_hw/script.js
@@ -1,37 +1,13 @@
-/* Задание на урок:
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-Проверить, чтобы все работало без ошибок в консоли */
-/* Задание на урок:
-
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять
-
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-
-4) Потренироваться и переписать цикл еще двумя способами*/
-
 "use strict";
-let numberOfFilms = +prompt("How many movies you seen?", "");
+let numberOfFilms;
+function start() {
+	numberOfFilms = +prompt("How many movies you seen?", "");
+	while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt("How many movies you seen?", "");
+	}
+}
+
+start();
 
 const personalMovieDB = {
 	count: numberOfFilms,
@@ -41,31 +17,49 @@ const personalMovieDB = {
 	privat: false,
 };
 
-// 	c = prompt("One at last seen movie?", ""),
-// 	d = prompt("What you rate it?", "");
+function rememberMyFilms() {
+	for (let i = 0; i < 2; i++) {
+		const a = prompt("That at last seen movie?", ""),
+			b = prompt("What you rate it?", "");
 
-// personalMovieDB.movies[c] = d;
-
-for (let i = 0; i < 2; i++) {
-	const a = prompt("That at last seen movie?", ""),
-		b = prompt("What you rate it?", "");
-
-	if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-		personalMovieDB.movies[a] = b;
-		console.log("done");
-	} else {
-		console.log("error");
-		i--;
+		if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+			personalMovieDB.movies[a] = b;
+			console.log("done");
+		} else {
+			console.log("error");
+			i--;
+		}
 	}
 }
 
-if (personalMovieDB.count < 10) {
-	console.log("To many little!");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-	console.log("You is classic movie watcher!");
-} else if (personalMovieDB.count >= 30) {
-	console.log("You is fan of movies!");
-} else {
-	console.log("Erorr!");
+rememberMyFilms();
+
+function detectPersonalLevel() {
+	if (personalMovieDB.count < 10) {
+		console.log("To many little!");
+	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+		console.log("You is classic movie watcher!");
+	} else if (personalMovieDB.count >= 30) {
+		console.log("You is fan of movies!");
+	} else {
+		console.log("Erorr!");
+	}
 }
-console.log(personalMovieDB);
+
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+	if (!hidden) {
+		console.log(personalMovieDB);
+	}
+}
+
+showMyDB();
+
+function writeYourGenres() {
+	for (let i = 1; i <= 3; i++) {
+		personalMovieDB.genres[i - 1] = prompt(`Your favorite genre number ${i}`);
+	}
+}
+
+writeYourGenres();
